2136b0956cffc0fa97c03675082a3da1bc810b36
Hsin Hsu <bar1eycool@gmail.com>
2020-04-30 14:39:02 +0800

    security update, format adjustment

--- SiteManagement.sol
+++ SiteManagement.sol
@@ -1,91 +1,106 @@
-pragma solidity ^0.4.0-^0.5.6;
-contract SiteManagement {
+pragma solidity ^0.5.17;
 
-	// a data sturcture is used to save each saved site's information.
-	struct Site {
-		address owner;		// registerSite function performer's ethereum address.
-		address addr;		// VerifiedSite contract ethereum address.
-		uint    timestamp;	// UNIX time format of writing time (block time).
-		bytes32 description;	// extra information given by user.
-	}
-  
-	// mapping sturcture (Key -> Site sturcture)
-	mapping (bytes32 => Site) regSites;	// regSites for registered site(s)
-	mapping (bytes32 => Site) unregSites;	// unregSites for unregistered site(s)
-	
-	// contract initialization.
-	constructor() public {
-	}
-	
-	/* registerSite is a function that write a VerifiedSite contract address and informations to global regSite
+
+contract SiteManagement {
+    // a data sturcture is used to save each saved site's information.
+    struct Site {
+        address owner; // registerSite function performer's ethereum address.
+        address addr; // VerifiedSite contract ethereum address.
+        uint256 timestamp; // UNIX time format of writing time (block time).
+        bytes32 description; // extra information given by user.
+    }
+
+    // mapping sturcture (Key -> Site sturcture)
+    mapping(bytes32 => Site) regSites; // regSites for registered site(s)
+    mapping(bytes32 => Site) unregSites; // unregSites for unregistered site(s)
+
+    // contract initialization.
+    constructor() public {}
+
+    /* registerSite is a function that write a VerifiedSite contract address and informations to global regSite
 	   input _fqdn: sites fully qualified domain name.
 		 _addr: sites VerifiedSite contract address.
 		 _description: information given by function user.
 	   output bool: ture if function success; false on the contrary.
 	*/
-	function registerSite(bytes32 _fqdn, address _addr, bytes32 _description) public returns (bool){
-		require(_fqdn > 0);
-		require(_addr > address(0));
-		require(_description >= 0);
-      
-		if (regSites[_fqdn].owner == address(0)) {
-		    // create new Site object
-			Site memory site = regSites[_fqdn];
-			// writing informaion
-			site.owner = msg.sender;
-			site.addr = _addr;
-			site.timestamp = now;
-			site.description = _description;
-			
-			return true;
-		} else {
-			return false;
-		}
-	}
-	/* unregisterSite is a function that remove a registered VerifiedSite contract from global regSite,
+    function registerSite(bytes32 _fqdn, address _addr, bytes32 _description)
+        public
+        returns (bool)
+    {
+        require(_fqdn > 0, "FQDN input error");
+        require(_addr > address(0), "VS addr input error");
+        require(_description >= 0, "description error");
+
+        if (regSites[_fqdn].owner == address(0)) {
+            // create new Site object
+            Site memory site = regSites[_fqdn];
+            // writing informaion
+            site.owner = msg.sender;
+            site.addr = _addr;
+            site.timestamp = now;
+            site.description = _description;
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /* unregisterSite is a function that remove a registered VerifiedSite contract from global regSite,
 	                  and dupicate a copy to unregSite for contract users to review.
 	   input _fqdn: sites fully qualified domain name that its owner wants to  .
 	   output bool: ture if function success; false on the contrary.
 	*/
-	function unregisterSite(bytes32 _fqdn) public returns (bool) {
-      	require(_fqdn > 0);
-		if (regSites[_fqdn].owner == msg.sender) {
-		    // dupicate and copy to unregSites
-			Site memory site = unregSites[_fqdn];
-			site.owner = msg.sender;
-			site.addr = regSites[_fqdn].addr;
-			site.timestamp = now; // unreg time
-			site.description = regSites[_fqdn].description;
-			
-			// wipe data by _fqdn index
-			regSites[_fqdn].owner = address(0);
-			regSites[_fqdn].addr = address(0);
-			regSites[_fqdn].timestamp = 0;
-			regSites[_fqdn].description = 0;
-          
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	/*
+    function unregisterSite(bytes32 _fqdn) public returns (bool) {
+        require(_fqdn > 0, "FQDN input error");
+        if (regSites[_fqdn].owner == msg.sender) {
+            // dupicate and copy to unregSites
+            Site memory site = unregSites[_fqdn];
+            site.owner = msg.sender;
+            site.addr = regSites[_fqdn].addr;
+            site.timestamp = now; // unreg time
+            site.description = regSites[_fqdn].description;
+
+            // wipe data by _fqdn index
+            regSites[_fqdn].owner = address(0);
+            regSites[_fqdn].addr = address(0);
+            regSites[_fqdn].timestamp = 0;
+            regSites[_fqdn].description = 0;
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
 	   Getter functions
 	*/
-	function getSiteAddr(bytes32 _fqdn) view public returns (address) {
-      	require(_fqdn > 0);
-		return regSites[_fqdn].addr;
-	}
-	function getSiteTimestamp(bytes32 _fqdn) view public returns (uint) {
-      	require(_fqdn > 0);
-		return regSites[_fqdn].timestamp;
-	}
-	function getSiteDescription(bytes32 _fqdn) view public returns (bytes32) {
-      	require(_fqdn > 0);
-		return regSites[_fqdn].description;
-	}
-	function getSiteInfo(bytes32 _fqdn) view public returns (address, uint, bytes32) {
-      	require(_fqdn > 0);
-	    return (regSites[_fqdn].addr, regSites[_fqdn].timestamp, regSites[_fqdn].description);
-	}
+    function getSiteAddr(bytes32 _fqdn) public view returns (address) {
+        require(_fqdn > 0, "FQDN input error");
+        return regSites[_fqdn].addr;
+    }
+
+    function getSiteTimestamp(bytes32 _fqdn) public view returns (uint256) {
+        require(_fqdn > 0, "FQDN input error");
+        return regSites[_fqdn].timestamp;
+    }
+
+    function getSiteDescription(bytes32 _fqdn) public view returns (bytes32) {
+        require(_fqdn > 0, "FQDN input error");
+        return regSites[_fqdn].description;
+    }
+
+    function getSiteInfo(bytes32 _fqdn)
+        public
+        view
+        returns (address, uint256, bytes32)
+    {
+        require(_fqdn > 0, "FQDN input error");
+        return (
+            regSites[_fqdn].addr,
+            regSites[_fqdn].timestamp,
+            regSites[_fqdn].description
+        );
+    }
 }
--- VerifiedSite.sol
+++ VerifiedSite.sol
@@ -1,105 +1,144 @@
-pragma solidity ^0.4.0-^0.5.6;
+pragma solidity ^0.5.17;
+
+
 contract VerifiedSite {
-	
-	/*
+    /*
 		a data sturcture is used to save each site's detection information.
 	*/
-	struct Detection {
-		address performer; 		// address of detection function performer.
-		uint expireTime; 		// UNIX time format of detection expire time.
-		bytes32 detectionTech;		// detect approach of current detection.
-		bytes32 detectionTechVersion;	// version of detect approach.
-		bool malicious;			// true if site is malicious, false on the contray.
-		bytes32 description;		// extra information given by user.
-	}
-	
-	/* current reward of the contract, set by requsetReview function.
-	   designed for improve contract users update information(detection).
-	*/
-	uint bounty;
-	
-	bytes32 public FQDN;  			// contract's Fully Qualified Domain Name.
-	uint public totalDetections;		// number of current sits's Detection(s).
-	uint public numMaliciousDetections;	// times that this site reported malicious.
-	bool public reviewable;			// state of this site is reviewable, change when initialization, requestReview and review.
-	
-	// mapping sturcture (Key -> Detection sturcture);
-	mapping (uint => Detection) public detections;
+    struct Detection {
+        address performer; // address of detection function performer.
+        uint256 expireTime; // UNIX time format of detection expire time.
+        bytes32 detectionTech; // detect approach of current detection.
+        bytes32 detectionTechVersion; // version of detect approach.
+        bool malicious; // true if site is malicious, false on the contray.
+        bytes32 description; // extra information given by user.
+    }
 
-	/* contract initialization.
-	   input _FQDN: 			// contract's Fully Qualified Domain Name.
-		 _detectionTech: 		// number of current sits's Detection(s).
-		 _detectionTechVersion: 	// times that this site reported malicious.
-		 _malicious:			// true if site is malicious, false on the contray.
-		 _description:			// extra information given by user.
+    /*
+		current reward of the contract, set by requsetReview function.
+		designed for improve contract users update information(detection).
 	*/
-	constructor(bytes32 _FQDN, bytes32 _detectionTech, bytes32 _detectionTechVersion, bool _malicious, bytes32 _description) public {
-		// init global variables
-		FQDN = _FQDN;
-		totalDetections = 0;
-		reviewable = false;
-		
-		// create new Detection object.
-		Detection storage det = detections[totalDetections++];
-		det.performer = msg.sender;
-		det.expireTime = block.timestamp + 86400; // expiredTime increase 86400 second(1 Day).
-		det.detectionTech = _detectionTech;
-		det.detectionTechVersion = _detectionTechVersion;
-		det.malicious = _malicious;
-		if (_malicious == true) {
-		    numMaliciousDetections += 1;
-		}
-		det.description = _description;
-	}
-	
-	/* requestReview is a function that users can put their ether into thin contract and inprove other user's motivation to
-			 review(perform another detect).
+    bytes32 public FQDN; // contract's Fully Qualified Domain Name.
+    uint256 public totalDetections; // number of current sits's Detection(s).
+    uint256 public numMaliciousDetections; // times that this site reported malicious.
+    bool public reviewable; // state of this site is reviewable, change when initialization, requestReview and review.
+    address payable ownerAddress; // Owner's address.
+    address payable webAddress; // assume that every web has its own ethereum address.
+
+    // mapping sturcture (Key -> Detection sturcture);
+    mapping(uint256 => Detection) public detections;
+
+    modifier onlyOwner {
+        require(ownerAddress == msg.sender, "not owner");
+        _;
+    }
+
+    modifier onlyRequester {
+        require(webAddress == msg.sender, "not requester");
+        _;
+    }
+
+    /*
+		Contract initialization.
+		Input:
+		_FQDN: 					// contract's Fully Qualified Domain Name.
+		_detectionTech: 		// number of current sits's Detection(s).
+		_detectionTechVersion: 	// times that this site reported malicious.
+		_malicious:				// true if site is malicious, false on the contray.
+		_description:			// extra information given by user.
+		_webAddress				// web's ethereum address
 	*/
-	function requestReview() payable public {
-		// an request can only success when reviewable is false and donated ether between 0.01 and 1.
-		if (reviewable == false && (msg.value <= 100000000000000000 && msg.value >= 1000000000000000)) {
-			// increase bounty by donated amount of ether.
-			bounty += msg.value;
-			// change review state
-			reviewable = true;
-		} else {
-			// return donated ether with message.
-			revert("Return ether");
-		}
-	}
-	/* review is a function that can only performed when reviewable global variable and timestamp is not expired. Then user can
-		  update detections and collect reward(bounty).
-		  Use Checks-Effects-Interactions(CEI) pattern to avoid reentrancy.
-	   input _detectionTech: number of current sits's Detections.
-		 _detectionTechVersion: times that this site reported malicious.
-		 _malicious: true if site is malicious, false on the contray.
-		 _description: information given by function user.
+
+    constructor(
+        bytes32 _FQDN,
+        bytes32 _detectionTech,
+        bytes32 _detectionTechVersion,
+        bool _malicious,
+        bytes32 _description,
+        address payable _webAddress
+    ) public {
+        // init global states
+        ownerAddress = msg.sender;
+        FQDN = _FQDN;
+        totalDetections = 0;
+        reviewable = false;
+
+        // create new Detection object.
+        Detection storage det = detections[totalDetections++];
+        det.performer = msg.sender;
+        det.expireTime = block.timestamp + 86400; // expiredTime increase 86400 second(1 Day).
+        det.detectionTech = _detectionTech;
+        det.detectionTechVersion = _detectionTechVersion;
+        det.malicious = _malicious;
+        webAddress = _webAddress;
+        det.description = _description;
+
+        if (_malicious == true) {
+            numMaliciousDetections += 1;
+        }
+    }
+
+    /*
+		requestReview is a function that users can put their ether into thin contract and inprove
+		other user's motivation to review(perform another detect).
 	*/
-	function review(bytes32 _detectionTech, bytes32 _detectionTechVersion, bool _malicious, bytes32 _description) public {
-		/* Checks 
-		   Check reviewable global variable and timestamp is not expired, throw an exception if the condition is not met.
+
+    function requestReview(uint256 amount) public payable onlyRequester {
+        require(msg.value == amount, "ether amount dismatch");
+        // an request can only success when reviewable is false and donated ether between 0.01 and 1.
+        if (reviewable == false) {
+            // change review state
+            reviewable = true;
+        } else {
+            // return donated ether with message.
+            revert("Return ether");
+        }
+    }
+
+    /*
+		review is a function that can only performed when reviewable global variable and timestamp
+		is not expired. Then user can update detections and collect reward. Use
+		Checks-Effects-Interactions(CEI) pattern to avoid reentrancy.
+	   	Input:
+	   	_detectionTech: number of current sits's Detections.
+		_detectionTechVersion: times that this site reported malicious.
+		_malicious: true if site is malicious, false on the contray.
+		_description: information given by function user.
+	*/
+
+    function review(
+        bytes32 _detectionTech,
+        bytes32 _detectionTechVersion,
+        bool _malicious,
+        bytes32 _description
+    ) public onlyOwner {
+        /* Checks
+		   	Check reviewable global variable and timestamp is not expired, throw an exception if the condition is not met.
+		   	* use by owner(contract constructor) only
 		*/
-		require(reviewable || (block.timestamp >= detections[totalDetections].expireTime)); 
-		
-		/* Effects */
-		// Same as Line 43-52
-		Detection memory det = detections[totalDetections++];
-		det.performer = msg.sender;
-		det.expireTime = block.timestamp + 86400;
-		det.detectionTech = _detectionTech;
-		det.detectionTechVersion = _detectionTechVersion;
-		det.malicious = _malicious;
-		if (_malicious == true) {
-		    numMaliciousDetections += 1;
-		}
-		det.description = _description;
-		
-		// change states
-		uint256 share = bounty;		// Create temp variable to save current bounty amount.
-		bounty = 0;
-		reviewable = false;
+        require(
+            reviewable ||
+                (block.timestamp >= detections[totalDetections].expireTime),
+            "not reviewable"
+        );
+        /* Effects */
+        Detection memory det = detections[totalDetections++];
+        det.performer = msg.sender;
+        det.expireTime = block.timestamp + 86400;
+        det.detectionTech = _detectionTech;
+        det.detectionTechVersion = _detectionTechVersion;
+        det.malicious = _malicious;
+        if (_malicious == true) {
+            numMaliciousDetections += 1;
+        }
+        det.description = _description;
+        reviewable = false;
 
-		/* Interactions */
-    		msg.sender.transfer(share);	// You get what you pay for!
-	}
+        /* Interactions */
+        ownerAddress.transfer(address(this).balance); // You get what you pay for!
+    }
+
+    function withdraw() public onlyRequester {
+        webAddress.transfer(address(this).balance);
+    }
 }
